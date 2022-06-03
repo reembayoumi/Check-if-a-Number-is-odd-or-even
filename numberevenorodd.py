c14dff268d13590f0edcca4f239f68e17c5cc66e
reembayoumi <reem.bayoumi@gmail.com>
2022-06-03 06:35:11 -1100

    Update numberevenorodd.py

--- numberevenorodd.py
+++ numberevenorodd.py
@@ -2,14 +2,8 @@
 # A number is even if division by 2 gives a remainder of 0.
 # If the remainder is 1, it is an odd number.
 
-# This program adds two numbers
-
 num = 4
-
 if (num % 2) == 0:
    print("{0} is Even".format(num))
 else:
    print("{0} is Odd".format(num))
-
-# Display the sum
-print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
